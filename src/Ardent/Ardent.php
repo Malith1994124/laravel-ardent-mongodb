d515e40e839cb329a1aa2e646676da5c39f3eead
Malith Pamuditha Fernando <malith124@gmail.com>
2024-09-13 16:39:41 +0000

    Fixed morphTo error

--- src/Ardent/Ardent.php
+++ src/Ardent/Ardent.php
@@ -468,7 +468,7 @@ abstract class Ardent extends Model {
 	 * @param  string  $id
 	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
 	 */
-	public function morphTo($name = null, $type = null, $id = null) {
+	public function morphTo($name = null, $type = null, $id = null, $ownerKey = null) {
 
 		// If no name is provided, we will use the backtrace to get the function name
 		// since that is most likely the name of the polymorphic interface. We can
